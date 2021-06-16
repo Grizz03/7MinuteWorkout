2a2d153aead59d3e861fbba07549475bcb25be38
grizz03 <sawilson1110@yahoo.com>
2021-06-16 00:23:39 -0700

    Added onClick Toast to button to test

--- app/src/main/java/me/stevew/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/me/stevew/a7minuteworkout/MainActivity.kt
@@ -1,11 +1,17 @@
 package me.stevew.a7minuteworkout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        llStart.setOnClickListener {
+            Toast.makeText(this, "Exercise will start now", Toast.LENGTH_SHORT).show()
+        }
     }
 }

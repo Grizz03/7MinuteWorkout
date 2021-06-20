edd2216a389aaaff7a078456c30aa9af468a432d
grizz03 <sawilson1110@yahoo.com>
2021-06-20 14:17:02 -0700

    updated..

--- app/src/main/java/me/stevew/a7minuteworkout/Constants.kt
+++ app/src/main/java/me/stevew/a7minuteworkout/Constants.kt
@@ -2,44 +2,47 @@ package me.stevew.a7minuteworkout
 
 class Constants {
     // companion == static
-    companion object{
+    companion object {
         fun defaultExerciseList(): ArrayList<ExerciseModel> {
+
             var exerciseList = ArrayList<ExerciseModel>()
 
             val jumpingJacks = ExerciseModel(1, "Jumping Jacks", R.drawable.ic_jumping_jacks, false, false)
             exerciseList.add(jumpingJacks)
 
-            val abdominalCrunch = ExerciseModel(1, "Abdominal Crunches", R.drawable.ic_abdominal_crunch, false, false)
+            val abdominalCrunch = ExerciseModel(2, "Abdominal Crunches", R.drawable.ic_abdominal_crunch, false, false)
             exerciseList.add(abdominalCrunch)
 
-            val highKnee = ExerciseModel(1, "High Knee Running In Place", R.drawable.ic_high_knees_running_in_place, false, false)
+            val highKnee =
+                ExerciseModel(3, "High Knee Running In Place", R.drawable.ic_high_knees_running_in_place, false, false)
             exerciseList.add(highKnee)
 
-            val lunge = ExerciseModel(1, "Lunges", R.drawable.ic_lunge, false, false)
+            val lunge = ExerciseModel(4, "Lunges", R.drawable.ic_lunge, false, false)
             exerciseList.add(lunge)
 
-            val plank = ExerciseModel(1, "Planks", R.drawable.ic_plank, false, false)
+            val plank = ExerciseModel(5, "Planks", R.drawable.ic_plank, false, false)
             exerciseList.add(plank)
 
-            val pushUp = ExerciseModel(1, "Push Ups", R.drawable.ic_push_up, false, false)
+            val pushUp = ExerciseModel(6, "Push Ups", R.drawable.ic_push_up, false, false)
             exerciseList.add(pushUp)
 
-            val pushUpRotation = ExerciseModel(1, "Push Ups With Rotation", R.drawable.ic_push_up_and_rotation, false, false)
+            val pushUpRotation =
+                ExerciseModel(7, "Push Ups With Rotation", R.drawable.ic_push_up_and_rotation, false, false)
             exerciseList.add(pushUpRotation)
 
-            val sidePlank = ExerciseModel(1, "Side Planks", R.drawable.ic_side_plank, false, false)
+            val sidePlank = ExerciseModel(8, "Side Planks", R.drawable.ic_side_plank, false, false)
             exerciseList.add(sidePlank)
 
-            val squat = ExerciseModel(1, "Squats", R.drawable.ic_squat, false, false)
+            val squat = ExerciseModel(9, "Squats", R.drawable.ic_squat, false, false)
             exerciseList.add(squat)
 
-            val stepUps = ExerciseModel(1, "Step Up Onto Chair", R.drawable.ic_step_up_onto_chair, false, false)
+            val stepUps = ExerciseModel(10, "Step Up Onto Chair", R.drawable.ic_step_up_onto_chair, false, false)
             exerciseList.add(stepUps)
 
-            val tricepDip = ExerciseModel(1, "Tricep Dips", R.drawable.ic_triceps_dip_on_chair, false, false)
+            val tricepDip = ExerciseModel(11, "Tricep Dips", R.drawable.ic_triceps_dip_on_chair, false, false)
             exerciseList.add(tricepDip)
 
-            val wallSit = ExerciseModel(1, "Wall Sit", R.drawable.ic_wall_sit, false, false)
+            val wallSit = ExerciseModel(12, "Wall Sit", R.drawable.ic_wall_sit, false, false)
             exerciseList.add(wallSit)
 
             return exerciseList
--- app/src/main/java/me/stevew/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/me/stevew/a7minuteworkout/MainActivity.kt
@@ -2,11 +2,11 @@ package me.stevew.a7minuteworkout
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

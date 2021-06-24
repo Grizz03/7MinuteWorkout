84ef70d39bb19409719eb0ab247f52fe019f6975
grizz03 <sawilson1110@yahoo.com>
2021-06-23 18:31:22 -0700

    indentation

--- app/src/main/java/me/stevew/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/me/stevew/a7minuteworkout/MainActivity.kt
@@ -6,12 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Opens new activity
+        // Opens new activity on button click
         llStart.setOnClickListener {
             val intent = Intent(this, ExerciseActivity::class.java)
             startActivity(intent)
